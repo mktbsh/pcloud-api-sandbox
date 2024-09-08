9d84fe574eeadc5c0c8da93959187175654811a2
mktbsh <t93.hasebe@gmail.com>
2024-09-08 19:38:53 +0900

    [remove] 不要な画像、コードを削除

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+async function main() {
+  const rootEl = document.getElementById("root");
+  if (!rootEl) throw new Error("mount target element is not found");
+
+  const root = createRoot(rootEl);
+
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
+
+main();
