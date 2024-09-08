67b1ac2f481080d3de336fe1bec4e04517bd3282
mktbsh <t93.hasebe@gmail.com>
2024-09-08 19:46:01 +0900

    [add] tanstack-router導入

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,29 @@
+import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
 
+import { routeTree } from "./routeTree.gen";
+
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+	interface Register {
+		router: typeof router;
+	}
+}
+
 async function main() {
-  const rootEl = document.getElementById("root");
-  if (!rootEl) throw new Error("mount target element is not found");
+	const rootEl = document.getElementById("root");
+	if (!rootEl) throw new Error("mount target element is not found");
 
-  const root = createRoot(rootEl);
+	const root = createRoot(rootEl);
 
-  root.render(
-    <StrictMode>
-      <App />
-    </StrictMode>
-  );
+	root.render(
+		<StrictMode>
+			<RouterProvider router={router} />
+		</StrictMode>,
+	);
 }
 
 main();
